9a33968cbfe13c0eb46aa5619a38aec1f742e694
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-09-16 15:03:47 +0200

    ajout d'un projet gestion destribution de mention en fct de la moyene

--- Apprentissage-python.py
+++ Apprentissage-python.py
@@ -1,3 +1,16 @@
 celsius = float(input ("Entrz une température en celsius : ") )
 fahrenheit = celsius * 9/5 +32
 print (celsius, "degrés Celsius équivalent à", fahrenheit, "degrés fahrenheit")
+
+moyenne = float(input("Quelle est votre moyenne"))
+
+if 12 <= moyenne < 14:
+    print("Assez bien")
+elif 14 <= moyenne < 16:
+    print("Bien")
+elif 16 <= moyenne < 18:
+    print("Tres bien")
+elif moyenne >= 18:
+    print("Félicitations")
+else:
+    print("Pas de mention")
