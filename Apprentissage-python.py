5bfd1370574b64cda20862c1de2fed04d54806e8
Benjamin Mazars <mazars.benjamin@hotmail.fr>
2024-09-16 15:21:35 +0200

    ajout du jeu du pendu

--- Apprentissage-python.py
+++ Apprentissage-python.py
@@ -14,3 +14,24 @@ elif moyenne >= 18:
     print("Félicitations")
 else:
     print("Pas de mention")
+
+nb_vies = 7
+mot_mystere = "python"
+mot_public = "_"*len(mot_mystere)
+
+while nb_vies > 0 and mot_mystere != mot_public :
+    lettre = str(input("Propose une lettre : "))
+    if (lettre) in mot_mystere :
+        for i in range(len(mot_mystere)) :
+            if mot_mystere[i] == lettre :
+                mot_public = mot_public[:i] + lettre + mot_public[i + 1:]
+    else :
+        nb_vies -= 1
+    if mot_public == mot_mystere:
+        print("Bravo ! Le mot est", mot_mystere)
+    elif nb_vies ==0 :
+        print("vous avez perdu")
+    else :
+        print("vous avez", nb_vies)
+        print("le mot est :", mot_public)
+
